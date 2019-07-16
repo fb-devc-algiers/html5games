401c080c930408a7b5eb635bcbd6f298fcf1fd51
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-07-16 15:18:39 +0100

    scaling

--- mygame/src/index.mjs
+++ mygame/src/index.mjs
@@ -1,12 +1,33 @@
 import {LoadImage} from './assets.mjs';
 
+
 const canvas = document.getElementById('main-canvas');
 const context = canvas.getContext('2d');
 
-//draw a shape
+//1-draw a shape
 context.fillRect(25, 25, 100, 100);
 
-//draw an image
+//2-draw an image
 LoadImage('../img/ninja2.png').then(img => {
     context.drawImage(img, 25, 35);
+});
+
+//3-drawing a background
+context.fillStyle = '#e2fcbf';
+context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+//3-Positioning and Resizing the Canvas
+const VIEWPORT_HEIGHT = 1080;
+const VIEWPORT_WIDTH = 720;
+const SCREEN_VIEWPORT_RATIO = window.innerHeight / VIEWPORT_HEIGHT;
+canvas.setAttribute('height', window.innerHeight);
+canvas.setAttribute('width', VIEWPORT_WIDTH *
+SCREEN_VIEWPORT_RATIO);
+
+//3-Scaling Content on the Canvas
+LoadImage('../img/ninja2.png').then(img => {
+    context.drawImage(
+        img,
+        25 * SCREEN_VIEWPORT_RATIO,
+        128 * SCREEN_VIEWPORT_RATIO);
 });
