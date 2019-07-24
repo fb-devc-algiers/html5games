c7ceade227bc8849bde10bd73885d7afe75a688a
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-07-24 14:59:49 +0100

    update

---
+++ 0-drawing/main.js
@@ -0,0 +1,31 @@
+var canvas = document.querySelector('canvas');
+//Set the canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//Create 2d rendering context
+var ctx = canvas.getContext('2d');
+
+//Draw a rectangle
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(100,100,100,100);
+
+//Draw rectangle 2
+ctx.fillStyle = 'rgba(0,0,255,0.5)';
+ctx.fillRect(100,400,100,100);
+
+//Draw rectangle 3
+ctx.fillStyle = 'rgba(0,255,0,0.5)';
+ctx.fillRect(300,300,100,100);
+
+//Draw a line
+ctx.beginPath();
+ctx.moveTo(50,300);
+ctx.lineTo(300,100);
+ctx.lineTo(400,300);
+ctx.strokeStyle = "#fa34a3";
+ctx.stroke();
+
+//Draw a circle
+ctx.arc(300,300,30,0,Math.PI*2,false);
+ctx.stroke();
---
+++ 1-a-animation/main.js
@@ -0,0 +1,50 @@
+var canvas = document.querySelector('canvas');
+//Set the canvas dimensions
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+//Create 2d rendering context
+var ctx = canvas.getContext('2d');
+
+//the circle initial position
+var x = 50;
+var y = 50;
+
+//the circle size
+var radius = 30;
+
+//the circle moves speed
+var speedx = 8;
+var speedy = 8;
+
+
+//animation routine
+function animate(){
+    //method the perform animation in the browser
+    //calling the same function to make an infinite recursive motion
+    requestAnimationFrame(animate);
+
+    //erase the defined area
+    ctx.clearRect(0,0,innerWidth,innerHeight);
+
+    //move the circle position
+    if(x + radius > innerWidth || x - radius < 0){
+        speedx = -speedx;
+    }
+    x += speedx;
+
+    if(y + radius > innerHeight || y - radius < 0){
+        speedy = -speedy;
+    }
+    y += speedy;
+
+    //draw the circle
+    ctx.beginPath();
+    ctx.arc(x,y,radius,0,Math.PI * 2, false);
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
+    ctx.fillStyle = 'rgba(0,255,0,0.5)';
+    ctx.fill();
+}
+
+//run the function animate
+animate();
